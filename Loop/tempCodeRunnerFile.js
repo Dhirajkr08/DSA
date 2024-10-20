a6525f61561447f0c92243702d3bc034004a2ae5
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-20 20:33:26 +0530

    reverse the number

---
+++ Loop/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+function reverseNumber(num){
+//     let reverse=0
+//     for(let i=1;i<=num;i++){
+//         i=num%10
+//         reverse=reverse*10+i
+//         num=Math.floor(num/10)
+//     }
+//     return reverse
+// }
+// let num=123
+// console.log(reverseNumber(num))
