e7f75a31d0cfce075a234a19e0afda28ab65755c
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-20 20:18:42 +0530

    checking armStrong

--- Loop/armStrong.js
+++ Loop/armStrong.js
@@ -1,1 +1,13 @@
-function 
+function armstrong(n){
+    let num=n //storing the original value to match with sum
+    let sum=0
+    while(n>0) {
+        let temp= n%10
+        sum+=temp**3
+        n=Math.floor(n/10)
+
+    }
+    return (num===sum)
+}
+let n=153
+console.log(armstrong(n))
