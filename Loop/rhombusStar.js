a70a78f8bdfb63cb0f9ad8861544ee8fb8d7f2ac
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-21 16:00:52 +0530

    adding rhombus star pattern

---
+++ Loop/rhombusStar.js
@@ -0,0 +1,13 @@
+function printPattern(n){
+    for(let i=0;i<n;i++){
+        let str=''
+        for(let j=0;j<n-i;j++){
+            str+=' '
+        }
+        for(let k=0;k<n;k++){
+            str+='*'
+        }
+        console.log(str)
+    }
+}
+printPattern(5)
