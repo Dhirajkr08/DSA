81cc0c0855c41777ecf3a90565b264ac1cdcbdc4
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-21 17:23:57 +0530

    way to find minimum in Array

--- Array/minimumInArray.js
+++ Array/minimumInArray.js
@@ -1,1 +1,12 @@
 function minimumInarr(arr){
+    let mini=0
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<mini){
+            mini=arr[i]
+        }
+    }
+    return mini
+    
+}
+let arr=[5,6,2,9,-2]
+console.log(minimumInarr(arr))
