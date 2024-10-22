7f37a3c37f4921201e39d53a43df70f5a2a3033a
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-22 14:27:05 +0530

    adding bubble sort method for sorting in array

--- Array/rotatedArray.js
+++ Array/rotatedArray.js
@@ -1,10 +1,11 @@
 function rotatedArray(num,k){
     k=k%num.length;
-    let rotated=num.slice(-k).concat(num.slice(0,num.length-k))
+    let rotated=num.slice(-k).concat(num.slice(0,num.length-k));
     for(let i=0;i<num.length;i++){
         num[i]=rotated[i]
     }
     return num
+
 }
 let num=[1,2,3,4,5,6,7],k=3
 console.log(rotatedArray(num,k))
