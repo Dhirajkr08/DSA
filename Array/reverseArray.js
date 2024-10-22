b6705dc6636a0fc62b2196ccd9579f9ce19739d8
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-22 14:59:18 +0530

    reverse an array using for loop and recursion

--- Array/reverseArray.js
+++ Array/reverseArray.js
@@ -12,3 +12,26 @@ function reverseArray(arr){
 }
 let arr=[17, 32, 9, 5, 21, 14]
 console.log(reverseArray(arr))
+
+//recursion  method
+
+
+function reverseArray(jrr) {
+    return reverseArrayHelper(jrr, 0, jrr.length - 1);
+}
+
+function reverseArrayHelper(jrr, l, h) {
+    if (l >= h) {
+        return jrr;
+    }
+    
+    let temp = jrr[l];
+    jrr[l] = jrr[h];
+    jrr[h] = temp;
+
+    
+    return reverseArrayHelper(jrr, l + 1, h - 1);
+}
+
+let jrr = [17, 32, 9, 5, 21, 14];
+console.log(reverseArray(jrr));  
