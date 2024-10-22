6d145322e3b384f870334a776cb02b302770ff53
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-22 21:50:52 +0530

    interview round revision

--- test.js
+++ test.js
@@ -1,38 +1,34 @@
-async function display() {
-    try {
-        let response = await fetch("https://api.postalpincode.in/pincode/226001");
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        let ans = await response.json();
-        console.log('Fetched data:', ans);
-        
-        localStorage.setItem('response', JSON.stringify(ans));
-        console.log('Stored in localStorage:', localStorage.getItem('response'));
-    } catch (error) {
-        console.error("Error:", error);
-    }
+function display(){
+    fetch("https://api.postalpincode.in/pincode/226001")
+    .then(response=>response.json())
+    .then(ans=>{
+        console.log(ans)
+        localStorage.setItem("response",JSON.stringify(ans))
+    })
+    .catch(err=>{
+        console.log("something wrong",err)
+    })
 }
-
-display();
+display()
 
 
 
 //Write JavaScript code to select the second nested item 
-// (the <li> with data-id="2b") and change its text content to "Updated Item".
-// let list=document.querySelectorAll('.nested-item[data-id="2b"]')
-// list[0].textContent='Updated Item';
+//(the <li> with data-id="2b") and change its text content to "Updated Item".
+
+let list=document.querySelectorAll('.nested-item')
+list[1].textContent='Updated Item';
 
 
 // //write data in JSON
-{
-    "name": "Dhiraj",
-    "Vill": "har",
-    "isVar" : false,
-    "add": {
-        "post": "Patna",
-        "backGround": "Nothing"
+// {
+//     "name": "Dhiraj",
+//     "Vill": "har",
+//     "isVar" : false,
+//     "add": {
+//         "post": "Patna",
+//         "backGround": "Nothing"
 
-    }
-}
+//     }
+// }
 
