645f3e906618e0c54c8c07cf105b601442f6c4e3
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-23 18:13:59 +0530

    consecutive ones in array using count and maximum

--- Array/consecutiveOnes.js
+++ Array/consecutiveOnes.js
@@ -1,16 +1,16 @@
 function consecutiveOnes(arr){
-    let count=0
-    let maxi=0
+    let count=0;maxi=0;
     for(let i=0;i<arr.length;i++){
         if(arr[i]===1){
             count++;
-            maxi=Math.max(maxi,count)
+            maxi=Ma.max(maxi,count)
         }
         else{
             count=0
+
         }
     }
-    return count;
+    return maxi
 }
-let arr=[1,1,0,1,1,1]
+let arr=[16,17,4,3,5,2]
 console.log(consecutiveOnes(arr))
