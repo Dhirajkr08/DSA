b00bb1b5775fe139be997fbfb702bc47101ff53d
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-23 17:47:49 +0530

    consecutive 1's in an array

---
+++ Array/consecutiveOnes.js
@@ -0,0 +1,16 @@
+function consecutiveOnes(arr){
+    let count=0
+    let maxi=0
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===1){
+            count++;
+            maxi=Math.max(maxi,count)
+        }
+        else{
+            count=0
+        }
+    }
+    return count;
+}
+let arr=[1,1,0,1,1,1]
+console.log(consecutiveOnes(arr))
