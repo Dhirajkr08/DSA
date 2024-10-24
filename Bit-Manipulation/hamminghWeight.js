6fe07c5ea74c318acd90a41faf53322a043455d8
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-24 19:08:56 +0530

    adding Hamminghweight in bit Manipulation

---
+++ Bit-Manipulation/hamminghWeight.js
@@ -0,0 +1,13 @@
+function hamminghWeight(n){
+    n=n.toString(2)
+    let count=0
+    for(let i=0;i<n.length;i++){
+        if(n[i]==='1'){
+            count++
+        }
+    }
+    return count
+
+}
+let n= 0000000000000000000000000001011
+console.log(hamminghWeight(n))
