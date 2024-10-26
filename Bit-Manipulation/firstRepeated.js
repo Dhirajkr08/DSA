89374be9b81b8a580edaf91c8fde4646221fe3d4
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-26 18:12:06 +0530

    getting first repeated character in string

---
+++ Bit-Manipulation/firstRepeated.js
@@ -0,0 +1,19 @@
+function firstRepeated(s){
+    let char={}
+    for(let i of s){
+        if(i in char){
+            char[i] ++
+        }
+        else{
+            char[i] = 1
+        }
+    }
+    for(let i of s){
+        if(char[i]>1){
+            return i
+        }
+    }
+    return '#'
+}
+let s="sharpener"
+console.log(firstRepeated(s))
