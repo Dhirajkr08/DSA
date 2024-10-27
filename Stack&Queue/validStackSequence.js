c07f4b9fd89e6d41aabb26c2646ee4068b7da8c6
Dhiraj08 <dhirajkr1117@gmail.com>
2024-10-28 01:18:45 +0530

    two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop operations on an initially empty stack, or false otherwise

---
+++ Stack&Queue/validStackSequence.js
@@ -0,0 +1,14 @@
+function validStack(pushed,popped){
+    let stack=[]
+    for(let i of pushed){
+        stack.push(i)
+        while(stack.length && stack[stack.length-1]=== popped[0]){
+            stack.pop()
+            popped.shift()
+        }
+    }
+    return stack.length===0
+}
+let pushed=[1,2,3,4,5]
+let popped=[4,5,3,2,1]
+console.log(validStack(pushed,popped))
