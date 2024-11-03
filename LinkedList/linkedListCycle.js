35a90abc9c4b16f6243ba2fefe962e60d52f96a7
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-04 04:06:40 +0530

    linked list cyclehave to return the node where the cycle begins

---
+++ LinkedList/linkedListCycle.js
@@ -0,0 +1,17 @@
+function linkedListCycle(head){
+    if(!head || !head.next)return null
+    let slow=head,fast=head
+    while(fast && fast.next){
+        slow=slow.next
+        fast =fast.next.next
+        if(slow===fast){
+            slow=head
+            while(slow  !== fast){
+                slow=slow.next
+                fast=fast.next
+            }
+            return slow
+        }
+    }
+    return -1
+}
