64fd6ee94da11f919a37b96e0fd960712a657105
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-05 21:32:53 +0530

    linked list cycle

--- LinkedList/linkedListCycle.js
+++ LinkedList/linkedListCycle.js
@@ -1,12 +1,12 @@
 function linkedListCycle(head){
     if(!head || !head.next)return null
-    let slow=head,fast=head
+    let slow=head,fast=head;
     while(fast && fast.next){
         slow=slow.next
-        fast =fast.next.next
-        if(slow===fast){
+        fast=fast.next.next
+        if(slow === fast){
             slow=head
-            while(slow  !== fast){
+            while(slow !== fast){
                 slow=slow.next
                 fast=fast.next
             }
