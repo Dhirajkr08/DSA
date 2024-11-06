5aa766f0e695d0d8b828ec895258697042487a27
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-06 20:20:05 +0530

    max slide window of the leetcode

--- Stack&Queue/maxSlideWindow.js
+++ Stack&Queue/maxSlideWindow.js
@@ -1,10 +1,10 @@
 function maxSlideWindow(nums,k){
-    let ans=[],queue=[]
+    let queue=[],ans=[]
     for(let i=0;i<nums.length;i++){
-        if(queue.length>0 && queue[0]<i-k+1){
+        if(queue.length>=0 && queue[queue.length-1]<i-k+1){
             queue.shift()
         }
-        while(queue.length>0 && nums[queue[queue.length-1]]<nums[i]){
+        while(queue.length>=0 && nums[queue[queue.length-1]]<nums[i]){
             queue.pop()
         }
         queue.push(i)
