b7efb5ae3098d364633dcd5be9152b27bb6ea6ee
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-06 20:41:17 +0530

    sum using recursion

--- Recursion/sum.js
+++ Recursion/sum.js
@@ -1,11 +1,18 @@
 function sum(n){
+    if(n===1)return 1;
+    return n+sum(n-1)
+}
+let n=5 
+console.log(sum(n)) //output =15
+
+//as per question
+function sum(n){
     function total(i=0,ans=0){
-        if(i>=10  || i===n){
-            return ans;
+        if(i>=10 || i===n){
+            return ans
         }
         return total(i+2,ans+i)
     }
     return total()
 }
-let n=5
-console.log(sum(n))
+let m=5
