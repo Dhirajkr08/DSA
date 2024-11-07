188901cd4759715866448e5306cd8133d0ff4ef8
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-07 09:38:37 +0530

    reverse and straigth digit series

--- Recursion/practise.js
+++ Recursion/practise.js
@@ -1,11 +1,23 @@
+// count(1)
+// function count(i){
+//     if(i===6){
+//         return 
+//     }
+//     else{
+//         count(i+1)//put this up  & this will reverse the code
+//         console.log(i)
+        
+//     }
+// }
+
 count(1)
 function count(i){
     if(i===6){
         return 
     }
     else{
-        count(i+1)//put this up  & this will reverse the code
         console.log(i)
-        
+        count(i+1)
+        console.log(i)
     }
 }
