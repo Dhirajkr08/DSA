4f17ec4f6a777a3af35adf3bb6620161ee25bcf8
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-08 17:02:02 +0530

    permutation using recursion

--- Recursion/permutation.js
+++ Recursion/permutation.js
@@ -1,35 +1,14 @@
-// function permutation(a){
-//     let ans=[]
-//     if(a.length===0)return ['']
-//     for(let i=0;i<a.length;i++){
-//         let char=a.slice(0,i)+a.slice(i+1)
-//         let perms=permutation(char)
-//         for(let j of perms){
-//             ans.push(str[i]+j)
-//         }
-//     }
-//     return ans
-// }
-// let str='abc'
-// let output=permutation(str)
-// for(let i=0;i<output.length;i++){
-//     console.log(output[i])
-// }
-
 function getPermutation(str){
-    let result=[]
+    let arr=[]
     if(str.length===0)return ['']
     for(let i=0;i<str.length;i++){
         let char=str.slice(0,i)+str.slice(i+1)
         let perms=getPermutation(char)
         for(let j of perms){
-            result.push(str[i]+j)
+            arr.push(str[i]+j)
         }
     }
-    return result
+    return arr
 }
 let str='abc'
-let output=getPermutation(str)
-for(let i=0;i<output.length;i++){
-    console.log(output[i])
-}
+console.log(getPermutation(str))
