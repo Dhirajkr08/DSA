6ffbb486ec521d0b03aa13209dc53315eb7c35fe
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-11 19:50:06 +0530

    insertMiddle in linked List

---
+++ LinkedList/insertInSortedLinkedList.js
@@ -0,0 +1,23 @@
+class LinkedList{
+    constructor(val){
+        this.val = val;
+        this.next=null;
+    }
+}
+function insertInMiddle(head,k){
+    let newHead = new ListNode(k)
+    if(!head)return newHead
+    let len=1,temp=head
+    while(temp){
+        len++;
+        temp=temp.next
+    }
+    let mid=Math.floor(len/2)
+    temp=head
+    for(let i=1;i<mid;i++){
+        temp=temp.next
+    }
+    newHead.next=temp.next
+    temp.next=newHead
+    return head
+}
