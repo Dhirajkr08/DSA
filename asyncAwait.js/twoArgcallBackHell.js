3f0718d2443fc5fe8df7981035dcc313170996cd
Dhiraj08 <dhirajkr1117@gmail.com>
2024-11-18 18:06:48 +0530

    callback hell with two arguments

---
+++ asyncAwait.js/twoArgcallBackHell.js
@@ -0,0 +1,19 @@
+function stepOne(cb){
+    setTimeout(()=>{
+        console.log("Step 1")
+        cb("Message from step 1")
+    },2000)
+
+}
+function stepTwo(message,cb){
+    setTimeout(()=>{
+        console.log("Step 2 received:",message)
+        cb("All steps completed!")
+    },1000)
+}
+stepOne(function(stepOneMessage){
+    stepTwo(stepOneMessage,function(stepTwoMessage){
+        console.log(stepTwoMessage)
+    })
+
+})
