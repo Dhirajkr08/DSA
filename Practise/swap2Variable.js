9dc80f273a33ae1b548d5b4e6bd8b2c9fba6e68b
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-19 17:28:19 +0530

    revising the swap of 2 variable

---
+++ Practise/swap2Variable.js
@@ -0,0 +1,13 @@
+function swap(a,b){
+    // let temp=a
+    // a=b
+    // b=temp
+    //or
+    [a,b]=[b,a]
+
+    return [a,b]
+    // console.log("a:",a,"b:",b)
+
+}
+let a=3,b=4
+console.log(swap(a,b))
