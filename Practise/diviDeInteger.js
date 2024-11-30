6443982b87133a6681fb43f6f4bab1fecdf48635
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-30 21:05:52 +0530

    divide two integer

---
+++ Practise/diviDeInteger.js
@@ -0,0 +1,15 @@
+function divideInteger(dividend,divisor){
+    let divide=parseInt(dividend/divisor)
+    if(divide>2147483647){
+        return 2147483647
+    }
+    else if(divide< -2147483648){
+        return -2147483648
+    }
+    else{
+        return divide
+    }
+
+}
+let dividend=10,divisor=3
+console.log(divideInteger(dividend,divisor))
