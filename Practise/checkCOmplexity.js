e2f00fb793d9a9c1da0a6009dd35214a727e737e
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-01 18:15:11 +0530

    checking another time complexity

--- Practise/checkCOmplexity.js
+++ Practise/checkCOmplexity.js
@@ -6,3 +6,12 @@ for(let i=0;i<n;i++){
 }
 // i can check roughly it is O(n^2) but can you explain how as per me cause it is going through same loop and iterating n twice so i think
 // it is O(n^2)
+
+
+let i=0,j=0,k=0
+for(i=n/2;i<=n;i++){
+    for(j=2;j<=n;j=j*2){
+        k=k+n/2
+    }
+}
+// i think asnwer would be O(N Log N) cause n is iterating twice but seperately for inner loop than outer loop
