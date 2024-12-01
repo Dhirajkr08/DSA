254eb03108e90a9ea750f9796e36ad083f41c4c1
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-01 19:33:57 +0530

    sum using xor in bit manipulation

---
+++ Practise/noXOR.js
@@ -0,0 +1,10 @@
+// 
+function noXor(arr,n){
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum+=(n-(i+1))^arr[i]
+    }
+    return sum
+}
+let n=5,arr=[1,2,3,4,5]
+console.log(noXor(arr,n))
