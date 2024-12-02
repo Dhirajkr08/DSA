4575c88cadc2b3b4b9e0e8a4d658156bf91b8395
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-02 11:40:16 +0530

    checking pangram means whether sentence have 26 alphabet or not which is non repeated

--- Practise/pangram.js
+++ Practise/pangram.js
@@ -1,1 +1,12 @@
-function 
+function pangram(s){
+    s=s.toLowerCase();
+    let ans=new Set()
+    for(let i of s){
+        if(i>='a' && i<='z'){
+            ans.add(i)
+        }
+    }
+    return ans.size===26
+}
+let s="The quick brown fox jumps over the lazy dog"
+console.log(pangram(s))
