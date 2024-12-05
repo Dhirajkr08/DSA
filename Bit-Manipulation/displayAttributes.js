f334ede0f020806194eba67746461a6e7b352978
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-12-05 17:54:39 +0530

    adding two way of console

--- Bit-Manipulation/displayAttributes.js
+++ Bit-Manipulation/displayAttributes.js
@@ -5,7 +5,7 @@ class student{
     }
     printAttributes(){
         console.log(`Name: ${this.name} ,Roll_Number: ${this.roll_number}`)
-        console.log('Name:', this.name, "Roll:",this.roll_number)
+        console.log('Name:',this.name , "Roll:",this.roll_number)
     }
 }
 let s=new student('Raman',34)
